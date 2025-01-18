80cebaf9d55a119a33bddaa59d63842ae41b47c1
Jashank <jashankjain224@gmail.com>
2025-01-18 18:31:33 +0530

    debug

--- src/components/TeacherPage.jsx
+++ src/components/TeacherPage.jsx
@@ -28,9 +28,10 @@ function TeacherPage() {
   };
 
   const askQuestion = () => {
-    if (!question.trim() || options.some((o) => !o.value.trim())) return;
+    if (!question.trim() || options.some((o) => !o.value.trim())) {console.log("there is no question")return};
 
     socket.emit("askQuestion", { question, options });
+    console.log("asking question -->", question, options);
     setCanAsk(false);
     setShowResults(true);
     setTimer(60);
